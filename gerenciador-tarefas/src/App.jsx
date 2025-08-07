3e0556f158e5ef467e0ddb10fa0f006fb26ee2ba
RafaelSantos-96 <rafaelmartins_contato@hotmail.com>
2025-08-07 16:32:56 +0000

    feat: adicionar componente BarraProgresso para mostrar percentual de tarefas concluídas

--- gerenciador-tarefas/src/App.jsx
+++ gerenciador-tarefas/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import NovaTarefa from './components/NovaTarefa';
 import Tarefas from './components/Tarefas';
-
+import BarraProgresso from './components/BarraProgresso';
 
 
 
@@ -26,7 +26,7 @@ return (
 <h1>Gerenciador de Tarefas</h1>
 <NovaTarefa onAdicionar={adicionarTarefa} />
 <Tarefas tarefas={tarefas} onAlternarStatus={alternarStatus} />
-
+<BarraProgresso totalTarefas={tarefas.length} tarefasConcluidas={tarefas.filter(t => t.status).length} />
 <ul>
 {tarefas.map((tarefa) => (
 <li key={tarefa.id}>
